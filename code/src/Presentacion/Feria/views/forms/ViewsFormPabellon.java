221d9f209fb881a1ad46fed84cbd13caffacd152
MarcoAdinolfi <33350660+MarcoAdinolfi@users.noreply.github.com>
2018-04-04 16:02:19 +0200

    Creada Vista Form de Pabellón
    Al final tenemos que apartar el fichero en la carpeta de Pabellon, ahora està en la carpeta views de Feria

---
+++ code/src/Presentacion/Feria/views/forms/ViewsFormPabellon.java
@@ -0,0 +1,176 @@
+package Presentacion.Pabellon.views.forms;
+
+import Negocio.Pabellon.Tpabellon;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Date;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+
+public class ViewsFormPabellon extends JFrame {
+
+    private JPanel dialogPanel;
+    private JPanel contentPanel;
+    private JPanel contentPanel2;
+    private JLabel label1;
+    private JTextField textFieldAforo;
+    private JPanel contentPanel3;
+    private JLabel label2;
+    private JTextField textFieldM2Utiles;
+    private JPanel contentPanel4;
+    private JLabel label3;
+    private JTextField textFieldM2Tot;
+    private JPanel buttonBar;
+    private JButton createButtonForm;
+    private JButton cancelButton;
+    private JButton helpButton;
+
+    public ViewsFormPabellon() {
+        initComponents();
+        this.setBounds(100,100, 800,800);
+        this.setVisible(true);
+    }
+
+    private void createButtonFormActionPerformed() {
+        setVisible(false);
+        String aforo = textFieldNombre.getText();  //!!!!!! Ivàn, esto lo ponemos directamente int o
+                                                    // lo cogemos String del negocio y lo parseamos como con la fecha de feria?
+                                                    // Creo la segunda visto el getText()
+        String m2_utiles = textFieldNombre.getText();
+        String m2_totales = textFieldNombre.getText();
+        Tpabellon tPabellon = new Tpabellon(aforo, m2_utiles, m2_totales);
+    }
+
+    private void cancelButtonStateChanged() {
+
+    }
+
+    private void helpButtonActionPerformed() {
+
+    }
+
+    private void initComponents() {
+        dialogPanel = new JPanel();
+        contentPanel = new JPanel();
+        contentPanel2 = new JPanel();
+        label1 = new JLabel();
+        textFieldAforo = new JTextField();
+        contentPanel3 = new JPanel();
+        label2 = new JLabel();
+        textFieldM2Utiles = new JTextField();
+        contentPanel4 = new JPanel();
+        label3 = new JLabel();
+        textFieldM2Tot = new JTextField();
+        buttonBar = new JPanel();
+        createButtonForm = new JButton();
+        cancelButton = new JButton();
+        helpButton = new JButton();
+
+        //======== this ========
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+
+        //======== dialogPanel ========
+        {
+            dialogPanel.setBorder(new EmptyBorder(12, 12, 12, 12));
+
+            // JFormDesigner evaluation mark
+            dialogPanel.setBorder(new javax.swing.border.CompoundBorder(
+                    new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+                            "", javax.swing.border.TitledBorder.CENTER,
+                            javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+                            java.awt.Color.red), dialogPanel.getBorder())); dialogPanel.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+            dialogPanel.setLayout(new BorderLayout());
+
+            //======== contentPanel ========
+            {
+                contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+                //======== contentPanel2 ========
+                {
+                    contentPanel2.setLayout(new BoxLayout(contentPanel2, BoxLayout.X_AXIS));
+
+                    //---- label1 ----
+                    label1.setText("Aforo");
+                    contentPanel2.add(label1);
+                    contentPanel2.add(textFieldAforo);
+                }
+                contentPanel.add(contentPanel2);
+
+                //======== contentPanel3 ========
+                {
+                    contentPanel3.setLayout(new BoxLayout(contentPanel3, BoxLayout.X_AXIS));
+
+                    //---- label2 ----
+                    label2.setText("Metros cuadros utiles");
+                    contentPanel3.add(label2);
+                    contentPanel3.add(textFieldM2Utiles);
+                }
+                contentPanel.add(contentPanel3);
+
+                //======== contentPanel4 ========
+                {
+                    contentPanel4.setLayout(new BoxLayout(contentPanel4, BoxLayout.X_AXIS));
+
+                    //---- label3 ----
+                    label3.setText("Metros cuadros totales");
+                    contentPanel4.add(label3);
+                    contentPanel4.add(textFieldM2Tot);
+                }
+                contentPanel.add(contentPanel4);
+            }
+            dialogPanel.add(contentPanel, BorderLayout.CENTER);
+
+            //======== buttonBar ========
+            {
+                buttonBar.setBorder(new EmptyBorder(12, 0, 0, 0));
+                buttonBar.setLayout(new GridBagLayout());
+                ((GridBagLayout)buttonBar.getLayout()).columnWidths = new int[] {0, 85, 85, 80};
+                ((GridBagLayout)buttonBar.getLayout()).columnWeights = new double[] {1.0, 0.0, 0.0, 0.0};
+
+                //---- createButtonForm ----
+                createButtonForm.setText("Add");
+                createButtonForm.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        createButtonFormActionPerformed();
+                    }
+                });
+                buttonBar.add(createButtonForm, new GridBagConstraints(0, 0, 1, 1, 0.0, 0.0,
+                        GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                        new Insets(0, 0, 0, 5), 0, 0));
+
+                //---- cancelButton ----
+                cancelButton.setText("Cancel");
+                cancelButton.addChangeListener(new ChangeListener() {
+                    @Override
+                    public void stateChanged(ChangeEvent e) {
+                        cancelButtonStateChanged();
+                    }
+                });
+                buttonBar.add(cancelButton, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                        GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                        new Insets(0, 0, 0, 5), 0, 0));
+
+                //---- helpButton ----
+                helpButton.setText("Help");
+                helpButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        helpButtonActionPerformed();
+                    }
+                });
+                buttonBar.add(helpButton, new GridBagConstraints(3, 0, 1, 1, 0.0, 0.0,
+                        GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                        new Insets(0, 0, 0, 0), 0, 0));
+            }
+            dialogPanel.add(buttonBar, BorderLayout.SOUTH);
+        }
+        contentPane.add(dialogPanel, BorderLayout.CENTER);
+        pack();
+        setLocationRelativeTo(getOwner());
+    }
+}
