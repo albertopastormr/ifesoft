af473c7754b46cf013cde0e5a27d8db26f2b6dc1
Ruben Garcia Mateos <36675328+RubenGarciaMateos@users.noreply.github.com>
2018-04-08 19:28:16 +0200

    Test Tferia
    implementadas funciones en test Tferia

--- code/test/Negocio/Feria/TferiaTest.java
+++ code/test/Negocio/Feria/TferiaTest.java
@@ -1,13 +1,40 @@
 package Negocio.Feria;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class TferiaTest {
     @Test
-    public void set() throws Exception {
+    public void getDescription() throws Exception {
+    	Tferia feria = new Tferia();
+    	assertTrue(feria.getDescription() != null);
     }
 
     @Test
-    public void get() throws Exception {
+    public void getName() throws Exception {
+    	Tferia feria = new Tferia();
+    	assertTrue(feria.getName() != null);
     }
+    @Test
+    public void getiniDate() throws Exception {
+    	Tferia feria = new Tferia();
+    	assertTrue(feria.getIniDate() != null);
+    }
+    @Test
+    public void getEndDate() throws Exception {
+    	Tferia feria = new Tferia();
+    	assertTrue(feria.getEndDate() != null);
+    }
+    @Test
+    public void getActive() throws Exception {
+    	Tferia feria = new Tferia();
+    	assertTrue(feria.getActive() != null);
+    }
+    @Test
+    public void getId() throws Exception {
+    	Tferia feria = new Tferia();
+    	assertTrue(feria.getId() != -1);
+    }
+    
 }
