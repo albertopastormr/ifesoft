413b07d3e578acc278096eaa91574aa87625bf46
Pablolop002 <pablolop002@gmail.com>
2018-04-11 10:49:50 +0200

    Fix feria II

--- code/src/Negocio/Pabellon/ASPabellonImp.java
+++ code/src/Negocio/Pabellon/ASPabellonImp.java
@@ -10,26 +10,44 @@ import Integracion.Pabellon.DAOPabellon;
 import java.util.Collection;
 
 public class ASPabellonImp implements ASPabellon {
-
     public Integer create(Tpabellon pabellon) throws ASException, DAOException {
         int id = -1;
+        DAOPabellon daoPabellon = IFDAOPabellon.getInstance().generateDAOpabellon();
+        if (pabellon != null && pabellon.getTotal_m2() >= 0 && pabellon.getUtil_m2() >= 0 && pabellon.getCapacity() >= 0) {
+            try {
+                Tpabellon read = daoPabellon.readById(pabellon.getId());
+                if (read == null) {
+                    if (pabellon.getId() != -1 && pabellon.getTotal_m2() >= 0 && pabellon.getTotal_m2() >= pabellon.getUtil_m2())
+                        id = daoPabellon.create(pabellon);
+                    else
+                        throw new ASException("ERROR: Los datos del pabellon no son correctos.\n");
+                } else
+                    throw new ASException("ERROR: El pabellon " + pabellon.getId() + "ya existe.\n");
+            } catch (Exception ex) {
+                throw new ASException(ex.getMessage());
+            }
+        } else
+            throw new ASException("ERROR: No se han introducido los datos del pabellon.\n");
         return id;
     }
 
     public Integer drop(Tpabellon pabellon) throws ASException, DAOException {
+        int id = -1;
         DAOPabellon daoPabellon = IFDAOPabellon.getInstance().generateDAOpabellon();
-        if (pabellon != null && pabellon.getId() != -1 && pabellon.getTotal_m2() >= 0 && pabellon.getTotal_m2() >= pabellon.getUtil_m2()) {
+        if (pabellon != null) {
+            pabellon.setActive(false);
             try {
                 Tpabellon read = daoPabellon.readById(pabellon.getId());
-                if (read == null) {
-
-                }
+                if (read != null)
+                    id = daoPabellon.update(pabellon);
+                else
+                    throw new ASException("ERROR: El pabellon " + pabellon.getId() + " no existe.\n");
             } catch (Exception ex) {
-
+                throw new ASException(ex.getMessage());
             }
         } else
             throw new ASException("ERROR: No se han introducido los datos del pabellon.\n");
-        return null;
+        return id;
     }
 
     public Integer modify(Tpabellon pabellon) throws ASException, DAOException {
