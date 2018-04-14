d6ac5ef40d586c39a7a7d33fe18b2e4ab1015b26
Pablo López Veleiro <pablolop002@gmail.com>
2018-04-14 18:35:13 +0200

    Arreglo DAOFeria codigo duplicado

--- code/src/Integracion/Feria/DAOFeria.java
+++ code/src/Integracion/Feria/DAOFeria.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Integracion.Feria;
 
 import Exceptions.DAOException;
@@ -18,24 +17,3 @@ public interface DAOFeria {
 	boolean delete (Integer id) throws DAOException;
 	void deleteAll() throws DAOException;
 }
-=======
-package Integracion.Feria;
-
-import Exceptions.DAOException;
-import Negocio.Feria.Tferia;
-
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Date;
-
-public interface DAOFeria {
-	Integer create(Tferia tferia) throws SQLException, DAOException;
-	Collection<Tferia> readAll() throws DAOException;
-	Tferia readByName(String name) throws DAOException;
-	Tferia readById(Integer id) throws DAOException;
-	Collection<Tferia> readByDates(Date initDate, Date endDate) throws DAOException;
-	Integer update(Tferia tferia) throws DAOException;
-	boolean delete (Integer id) throws DAOException;
-	void deleteAll() throws DAOException;
-}
->>>>>>> 7ac6ce0a20d809961d99c3a350e9acb9ea955b82
