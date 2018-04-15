6517beeed2df9923da1be8519192bd5510375869
MeSeventy <artesz.amon@gmail.com>
2018-04-15 12:06:09 +0200

    ListAssignation GUI
    List assignation creado sin comprobar

--- code/src/Presentacion/views/shows/List/ListAssignation.java
+++ code/src/Presentacion/views/shows/List/ListAssignation.java
@@ -4,6 +4,8 @@ import Negocio.Asignacion.Tasignacion;
 import Negocio.Pabellon.Tpabellon;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.util.Collection;
 
 public class ListAssignation extends JFrame {
@@ -11,11 +13,14 @@ public class ListAssignation extends JFrame {
     String[] columnNames = {"ID FAIR","ID PAVILION","ID STAND","USED M2"};
     Object[][] data;
 
+    JTable table;
+
     Collection<Tasignacion> tAssignaction;
 
 
     public ListAssignation(Collection<Tasignacion> tassignation){
         super();
+        this.setLayout(new BorderLayout());
         this.tAssignaction = tassignation;
         this.initGUI();
     }
@@ -31,7 +36,24 @@ public class ListAssignation extends JFrame {
 
         changeTransferToCollection(tAssignaction);
 
-        new TableList(mainPanel, columnNames, data);
+        table = new JTable(data, columnNames);
+        DefaultTableModel tableModel = new DefaultTableModel() {
+
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                //all cells false
+                return false;
+            }
+        };
+
+        table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+
+        JScrollPane pane = new JScrollPane(table);
+        add(pane, BorderLayout.CENTER);
+
+
+        table.setModel(tableModel);
+        mainPanel.add(pane);
 
         this.add(mainPanel);
         this.pack();
