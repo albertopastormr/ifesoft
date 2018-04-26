abfd26ee8677a381cdd90c808aaf22817c049cff
Ruben Garcia Mateos <36675328+RubenGarciaMateos@users.noreply.github.com>
2018-04-27 00:35:57 +0200

    Mas cambios

--- code/test/Negocio/Asignacion/ASAsignacionImpTest.java
+++ code/test/Negocio/Asignacion/ASAsignacionImpTest.java
@@ -85,20 +85,13 @@ public class ASAsignacionImpTest {
     //Metodo donde comprobaremos que no se puede crear una asignacion con un ID de feria incorrecto
     @Test(expected = ASException.class)
     public void createAsignationWithIncorrectIdFair() throws SQLException, DAOException, ASException{
-        Integer idFair = -1, idPavilion = -1, idAsignation = -1;
+        Integer idFair = -1;
 
         ASAsignacionImp asAsignation = new ASAsignacionImp();
-        DAOAsignacion daoAsignation = IFDAOAsignacion.getInstance().generateDAOasignacion();
-        daoAsignation.deleteAll(); //Vaciamos la bbdd de asignaciones
-
-        //Generamos DAO y transfer para pabellon e insertarlo en la bbdd
-        DAOPabellon daoPavilion = IFDAOPabellon.getInstance().generateDAOpabellon();
-        Tpabellon transferPavilion = new Tpabellon(idPavilion, 5000, 5000, false);
-        idPavilion = daoPavilion.create(transferPavilion);
 
         //El id de feria será -1 ya que no se ha generado ninguna feria
         //4000m2 contratatdos o totales, 3000m2 asignados a es stand en ese pabellon
-        Tasignacion transferAsignation1 = new Tasignacion(idAsignation, idFair, idPavilion, 4000, 3000, false);
+        Tasignacion transferAsignation1 = new Tasignacion(ASAsignacionImpTest.tasignacionTest1.getId(), idFair, idPabellon1, 4000, 3000, false);
         asAsignation.create(transferAsignation1);
 
     }
@@ -106,48 +99,23 @@ public class ASAsignacionImpTest {
     //Metodo donde comprobaremos que no se puede crear una asignacion con un ID de pabellon incorrecto
     @Test(expected = ASException.class)
     public void createAsignationWithIncorrectIdPavilion() throws SQLException, DAOException, ASException{
-        Integer idFair = -1, idPavilion = -1, idAsignation = -1;
-
+        Integer idPavilion = -1;
         ASAsignacionImp asAsignation = new ASAsignacionImp();
-        DAOAsignacion daoAsignation = IFDAOAsignacion.getInstance().generateDAOasignacion();
-        daoAsignation.deleteAll(); //Vaciamos la bbdd de asignaciones
-
-        //Generamos DAO y transfer para feria para insertar una feria en la bbdd
-        DAOFeria daoFeria = IFDAOFeria.getInstance().generateDAOferia();
-        Date dateIni = new Date((4016-1900), 1, 12);
-        Date dateEnd = new Date((4016-1900), 1, 18);
-        Tferia transferFair = new Tferia(idFair, "IBM", "ExampleFair", dateIni, dateEnd, false);
-        idFair =  daoFeria.create(transferFair); //Creamos la feria y la insertamos en la bbdd
 
         //El id de pabellon será -1, ya que no se ha creado ningun pabellon
         //4000m2 contratatdos o totales, 3000m2 asignados a es stand en ese pabellon
-        Tasignacion transferAsignation1 = new Tasignacion(idAsignation, idFair, idPavilion, 4000, 3000, false);
+        Tasignacion transferAsignation1 = new Tasignacion(ASAsignacionImpTest.tasignacionTest1.getId(), idFeria1, idPavilion, 4000, 3000, false);
         asAsignation.create(transferAsignation1);
     }
 
     //Metodo donde comprobamos que los metros cuadrados de pabellon deben ser mas grandes que los contratados en la asignacion, luego saltara excepcion
     @Test(expected = ASException.class)
     public void createAsignationWithDataIncorrect() throws ASException, DAOException, SQLException{
-        Integer idFair = -1, idPavilion = -1, idAsignation = -1;
-
         ASAsignacionImp asAsignation = new ASAsignacionImp();
-        DAOAsignacion daoAsignation = IFDAOAsignacion.getInstance().generateDAOasignacion();
-        daoAsignation.deleteAll(); //Vaciamos la bbdd de asignaciones
 
-        //Generamos DAO y transfer para feria para insertar una feria en la bbdd
-        DAOFeria daoFeria = IFDAOFeria.getInstance().generateDAOferia();
-        Date dateIni = new Date((4016-1900), 1, 12);
-        Date dateEnd = new Date((4016-1900), 1, 18);
-        Tferia transferFair = new Tferia(idFair, "IBM", "ExampleFair", dateIni, dateEnd, false);
-        idFair =  daoFeria.create(transferFair); //Creamos la feria y la insertamos en la bbdd
-
-        //Generamos DAO y transfer para pabellon e insertarlo en la bbdd
-        DAOPabellon daoPavilion = IFDAOPabellon.getInstance().generateDAOpabellon();
-        Tpabellon transferPavilion = new Tpabellon(idPavilion, 5000, 5000, false);
-        idPavilion = daoPavilion.create(transferPavilion);
 
         //Los metros cuadrados que le introducimos al total de la asignacion son 40000, y deberían ser menores que los que tiene pabellon en la bbdd que son 5000
-        Tasignacion transferAsignation1 = new Tasignacion(idAsignation, idFair, idPavilion, 40000, 3000, false);
+        Tasignacion transferAsignation1 = new Tasignacion(ASAsignacionImpTest.tasignacionTest1.getId(), idFeria1, idPabellon1, 40000, 3000, false);
         asAsignation.create(transferAsignation1);
     }
     //---------------------------------------------------------------------------------------------------------------------------------
@@ -160,8 +128,6 @@ public class ASAsignacionImpTest {
         Integer asignationId = 223344;
 
         ASAsignacionImp asAsignation = new ASAsignacionImp();
-        DAOAsignacion daoAsignation = IFDAOAsignacion.getInstance().generateDAOasignacion();
-        daoAsignation.deleteAll(); //Vaciamos la bbdd de asignaciones
 
         Tasignacion transferAsignation = new Tasignacion(asignationId, 223344, 223344, 40000, 3000, true);
         asAsignation.drop(transferAsignation);
@@ -172,37 +138,31 @@ public class ASAsignacionImpTest {
     public void dropAsignationNull() throws ASException, SQLException, DAOException{
 
         ASAsignacionImp asAsignation = new ASAsignacionImp();
-        DAOAsignacion daoAsignation = IFDAOAsignacion.getInstance().generateDAOasignacion();
-        daoAsignation.deleteAll(); //Vaciamos la bbdd de asignaciones
 
         Tasignacion transferAsignation = null;
         asAsignation.drop(transferAsignation);
     }
 
-    @Test(expected = ASException.class) //Metodo donde queremos borrar una asignacion que supuestamente esta borrada ya, tiene el booleano a false
+    @Test(expected = ASException.class) //Metodo donde queremos borrar una asignacion que supuestamente esta borrada ya
     public void dropAsignationWithActiveFalse() throws ASException, SQLException, DAOException{
         int asignationId = -1;
         ASAsignacionImp asAsignation = new ASAsignacionImp();
-        DAOAsignacion daoAsignation = IFDAOAsignacion.getInstance().generateDAOasignacion();
-        daoAsignation.deleteAll(); //Vaciamos la bbdd de asignaciones
+        asignationId = asAsignation.create(tasignacionTest1); //Creamos la asignacion primero con sus datos y nos pondra el booleano a true
+        Tasignacion tasignacion = new Tasignacion(asignationId, 1, 1, 1, 1,true);
 
-        Tasignacion transferAsignation = new Tasignacion(asignationId, 223344, 223344, 40000, 3000, false);
-        asignationId = asAsignation.create(transferAsignation); //Creamos la asignacion primero con sus datos y nos pondra el booleano a true
-        asAsignation.drop(transferAsignation); //Aqui deberia saltar la excepcion ya que ya esta a false el booleano
+        asAsignation.drop(tasignacion);
+        asAsignation.drop(tasignacion); //Aqui deberia saltar la excepcion ya que ya esta a false el booleano, y no podemos borrar dos veces lo mismo
     }
 
-    //HAY QUE COMPROBAR SI SE EJECUT BIEN ESTE TEST YA QUE SE HARIA BORRADO LOGICO RECURSIVO DE LOS STANDS ASOCIADOS.
-    @Test(expected = ASException.class)
+    @Test
     //Metodo donde borramos correctamente una asignacion
     public void dropAsignationCorrectly() throws ASException, SQLException, DAOException{
         Integer asignationId = -1;
         ASAsignacionImp asAsignation = new ASAsignacionImp();
-        DAOAsignacion daoAsignation = IFDAOAsignacion.getInstance().generateDAOasignacion();
-        daoAsignation.deleteAll(); //Vaciamos la bbdd de asignaciones
+        asignationId = asAsignation.create(tasignacionTest1); //Creamos la asignacion
+        Tasignacion tasignacion = new Tasignacion(asignationId, 1, 1, 1, 1,true);
 
-        Tasignacion transferAsignation = new Tasignacion(asignationId, 223344, 223344, 40000, 3000, false);
-        asAsignation.create(transferAsignation); //Creamos la asignacion
-        assertTrue(asAsignation.drop(transferAsignation) > 0); //si es mayor que 0 se borra correctamente
+        assertTrue(asAsignation.drop(tasignacion) > 0); //si es mayor que 0 se borra correctamente
     }
     //----------------------------------------------------------------------------------------------------------------------------------
 
