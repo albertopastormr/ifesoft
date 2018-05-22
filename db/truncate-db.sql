13c7106e0815b333c3a502460e8f064f87d12246
albertopastormr <general.tyrus@hotmail.com>
2018-05-22 17:05:19 +0200

    agregado truncate db

---
+++ db/truncate-db.sql
@@ -0,0 +1,10 @@
+SET FOREIGN_KEY_CHECKS = 0;
+TRUNCATE TABLE stand;
+TRUNCATE TABLE feria;
+TRUNCATE TABLE asignacion;
+TRUNCATE TABLE participacion;
+TRUNCATE TABLE pabellon;
+TRUNCATE TABLE part_nacional;
+TRUNCATE TABLE part_internacional;
+TRUNCATE TABLE participante;
+SET FOREIGN_KEY_CHECKS = 1;
